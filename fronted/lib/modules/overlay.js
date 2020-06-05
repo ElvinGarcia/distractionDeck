8b5c170de09f4cec3286221486712d6081c2ec24
ElvinGarcia <dimix181@gmail.com>
2020-06-05 13:29:29 -0400

    prevented tmp files from being committed

---
+++ fronted/lib/modules/overlay.js
@@ -0,0 +1,9 @@
+
+
+ function test (comm){
+  console.log(comm)
+               };
+
+export{test} ;
+
+
---
+++ fronted/lib/scripts.js
@@ -0,0 +1,25 @@
+
+
+// import {test} from "./modules/overlay.js";
+import * as Module from './modules/overlay.js'
+// import { m } from './modules/message.js';
+
+Module.test("Doing what works Doing what matters");
+
+
+
+document.querySelector(".main-columns-list>ul").addEventListener("click", () => {
+ import("./modules/message.js").then((Module) => {
+  console.log(Module);
+ });
+});
+
+<div class="js-tweet tweet">
+      <header class="tweet-header js-tweet-header flex flex-row flex-align--baseline">  <a class="account-link link-complex block flex-auto" href="https://twitter.com/FastCompany" rel="user" target="_blank">
+           
+           <div class="obj-left item-img tweet-img position-rel">  <img class="tweet-avatar avatar pin-top-full-width" src="https://pbs.twimg.com/profile_images/1051826427837014017/v2TLU4mf_normal.jpg" alt="FastCompany's avatar">  </div> 
+           
+           <div class="nbfc "> <span class="account-inline txt-ellipsis"> <b class="fullname link-complex-target">Fast Company</b> <i class="js-show-tip sprite sprite-verified-mini" title="Verified account"></i> <span class="username txt-mute">@FastCompany</span>  </span> 
+           </div> </a>  <time class="tweet-timestamp js-timestamp txt-mute flex-shrink--0" datetime="2020-06-03T00:55:44.000Z" data-time="1591145744000">  <a class="txt-size-variable--12 no-wrap" href="https://twitter.com/FastCompany/status/1267983271738200065" rel="url" target="_blank">3m</a>  </time>  
+     </header>  
+     <div class="tweet-body js-tweet-body">   <div class="nbfc txt-size-variable--12 txt-ellipsis">  </div>   <p class="js-tweet-text tweet-text with-linebreaks " lang="en">There is a growing list of curfew orders as protests continue. <a href="https://t.co/fMaRWl39aH" target="_blank" class="url-ext" data-full-url="http://f-st.co/K5Ue4DJ" rel="url noopener noreferrer">f-st.co/K5Ue4DJ</a></p> <div class="js-card-container"></div>           </div>  <footer class="tweet-footer cf">   <ul class="js-tweet-actions tweet-actions full-width "> <li class="tweet-action-item pull-left margin-r--10"> <a class="js-reply-action tweet-action position-rel" href="#" rel="reply"> <i class="icon icon-reply txt-center pull-left"></i>  <span class="pull-right icon-reply-toggle margin-l--2 margin-t--1 txt-size--12 js-reply-count reply-count">2</span>  <span class="is-vishidden">Reply</span> <span class="reply-triangle"></span> </a> </li>  <li class="tweet-action-item pull-left margin-r--10 "> <a class="tweet-action " href="#" rel="retweet"> <i class="js-icon-retweet icon icon-retweet icon-retweet-toggle txt-center pull-left"></i>  <span class="pull-right icon-retweet-toggle margin-l--3 margin-t--1 txt-size--12 js-retweet-count retweet-count">2</span>  <span class="is-vishidden">Retweet</span> </a> </li>  <li class="tweet-action-item pull-left margin-r--10"> <a class="js-show-tip tweet-action position-rel" href="#" rel="favorite" title="" data-original-title="  Like from elvinthecreator   "> <i class="js-icon-favorite icon icon-favorite icon-favorite-toggle txt-center pull-left"></i>  <span class="pull-right icon-favorite-toggle margin-l--2 margin-t--1 txt-size--12 js-like-count like-count">2</span>  <span class="is-vishidden"> Like </span> </a> </li> <li class="tweet-action-item position-rel pull-left margin-r--10"> <a class="tweet-action" href="#" rel="actionsMenu" data-user-id="2735591" data-chirp-id="1267983271738200065" data-account-key="twitter:101623641" data-actions-menu-position="" data-parent-chirp-id=""> <i class="icon icon-more txt-right"></i> <span class="is-vishidden">More options</span> </a> </li>  <li class="feature-customtimelines tweet-drag-handle tweet-action-item pull-right margin-l--7"> <span class="tweet-action" href="https://twitter.com/FastCompany/status/1267983271738200065"> <i class="icon icon-move txt-right txt-right"></i> <span class="is-vishidden">Drag to Collection</span> </span> </li>   </ul>  </footer>  <div class="js-show-this-thread">    </div> </div>
