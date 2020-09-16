f8236d702d56e7a87176527389ca0cf7513e1137
ElvinGarcia <dimix181@gmail.com>
2020-09-16 13:51:44 -0400

    update rails app

--- distractionDeck-api/config/application.rb
+++ distractionDeck-api/config/application.rb
@@ -1,19 +1,19 @@
 require_relative 'boot'
 
-require 'rails'
+require "rails"
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
 # require "active_storage/engine"
-require 'action_controller/railtie'
+require "action_controller/railtie"
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require 'action_view/railtie'
-require 'action_cable/engine'
+require "action_view/railtie"
+require "action_cable/engine"
 # require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,16 +24,6 @@ module RedDeckApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # # sets a middleware for the API
-    # # Used to set cookies
-    # config.api_only = true
-    # config.middleware.use ActionDispatch::Cookies
-    # config.middleware.use ActionDispatch::Session::CookieStore
-
-    config.session_store :cookie_store, key: '_interslice_session'
-    config.middleware.use ActionDispatch::Cookies # Required for all session management
-    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -43,12 +33,5 @@ module RedDeckApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    # config.middleware.insert_before 0, Rack::Cors do
-    #   allow do
-    #     origins '*'
-    #     resource '*', headers: :any, methods: [:get, :post]
-    #   end
-    # end
   end
 end
