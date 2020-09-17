07fb74b4b724e452d50ee3c6da6c2d1061a862d6
ElvinGarcia <dimix181@gmail.com>
2020-09-16 20:56:40 -0400

    updates to overall application and elimination of omniauth

--- distractionDeck-api/config/routes.rb
+++ distractionDeck-api/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+     resources :posts
+     resources :columns
+     resources :users
+     resources :hastags
+     resources :comments
+    end
+  end
+
+   resources :login, to: 'sessions#login'
+  #  get '/auth/:provider/callback', to: 'sessions#login'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
