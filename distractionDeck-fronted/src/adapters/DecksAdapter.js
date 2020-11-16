4eba5f7a93ed55084e7a471f424942490fcf3cb4
ElvinGarcia <dimix181@gmail.com>
2020-11-16 10:16:02 -0500

    created POST request when submitting a new post

--- distractionDeck-fronted/src/adapters/DecksAdapter.js
+++ distractionDeck-fronted/src/adapters/DecksAdapter.js
@@ -21,4 +21,22 @@ class DecksAdapter {
       .then((response) => response.json())
       ;
   }
+
+  // left here making a request to RAILS API to submit a post
+  postRequest(obj) {
+    return fetch(this.baseUrl + "/postRequest", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(obj),
+    })
+      .then((response) => {
+       if (response.ok) {
+         return response;
+       } else {
+         throw `Error: ${response.status}`
+       }
+      })
+  }
+
+
 }
