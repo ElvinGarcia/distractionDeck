d3bac1d8d2147a90cd355a0e9b2f457f7990d4a3
ElvinGarcia <dimix181@gmail.com>
2020-12-19 14:47:27 -0500

    fixed user authentication validation when user is not found in DB

--- distractionDeck-api/app/controllers/sessions_controller.rb
+++ distractionDeck-api/app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   def login
     user = User.find_by(name: params[:username])
-    if user.authenticate(params[:password])
-      # log_in(user)  doesn't work
+    if user && user.authenticate(params[:password])
+
        render :json => UserSerializer.new(user).to_serialized_json, status: 200
     else
       render json: { alert: 'Invalid email/password combination' }, status: 401
